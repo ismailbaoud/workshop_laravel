1ee44d9a39b5026288d86f4125bde9775c94e979
ismailbaoud <ibaoud46@gmail.com>
2025-02-20 03:54:15 -0600

    fait la migration et preparer les donne avec faillabele

--- database/migrations/2025_02_20_092123_create_post_models_table.php
+++ database/migrations/2025_02_20_092123_create_post_models_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('description');
             $table->timestamps();
             $table->foreignId('user_id')->constrained('users');
-
         });
     }
 
